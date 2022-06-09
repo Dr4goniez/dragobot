14322be624fab7af45a4ca344e8c9552a8013ee1
dragoniez <dragoniez.jawp@gmail.com>
2022-06-10 01:02:50 +0900

    Fixed a typo

--- my.dist.js
+++ my.dist.js
@@ -1,4 +1,4 @@
 // This is a file that exemplifies how you can use your personal information in your script.
 exports.apiUrl = 'API endpoint';
 exports.username = 'USERNAME';
-exports.password = 'PASSOWORD';
+exports.password = 'PASSWORD';
