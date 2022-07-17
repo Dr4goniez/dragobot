b41dbf2b56bca218d1d67e1d5f94629901269a31
dragoniez <dragoniez.jawp@gmail.com>
2022-07-17 14:54:42 +0900

    v3.0.1: Test for server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
 module.exports.createLandingPage = () => {
-    try {
+    // try {
         const port = parseInt(process.env.PORT, 10);
         const html = require('fs').readFileSync('./index.html');
         http.createServer((req, res) => {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(html);
         }).listen(port);
-    } catch {}
+    // } catch {}
 };
